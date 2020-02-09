8f8bf974fec6030639473096218cec4195f3061e
victor fernandes <victorfernandes@MacBook-Pro-de-victor.local>
2020-02-09 17:57:13 -0300

    Initial commit

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    public void startGameButton() {
+        int i = 2;
+        string a = "robson";
+        Debug.Log(a);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
