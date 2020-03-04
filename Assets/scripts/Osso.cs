0f2a38954a0644301634b394da459f6d99565e56
Tatu Miagui <tatumiagui@Tatu.local>
2020-03-03 23:47:09 -0300

    renomeando arquivos de audio

--- Assets/scripts/Osso.cs
+++ Assets/scripts/Osso.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Osso : MonoBehaviour
 {
+    private bool isSelected = false;
+    private bool isChecked = false;
+
+
+    public void setSelected(bool value){
+        this.isSelected = value;
+        // adicionar código que troca imagem
+    }
+
+    public void setChecked(bool value){
+        this.isChecked = value;
+        // adicionar código que da checked
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
