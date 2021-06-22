69910a6c2d0cc57bfc0410ab4fe9cd41d68179a6
mauriciolfsilva <mauriciolfsilva@gmail.com>
2021-06-22 02:52:04 -0300

    Galeria

---
+++ Assets/scripts/Galeria.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Galeria : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject scroll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(prefab, new Vector3(50, -200 - (i * 600), 0), Quaternion.identity, scroll.transform);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
